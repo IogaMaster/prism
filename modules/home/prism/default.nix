314cd33297251488e49a191b7ecec14ed1858ce2
IogaMaster <iogamastercode@gmail.com>
2023-10-26 19:44:17 -0600

    Add first version of custom palette support

--- modules/home/prism/default.nix
+++ modules/home/prism/default.nix
@@ -12,7 +12,7 @@ in {
       description = ''
         The directory the the wallpapers are outputted to.
       '';
-      type = lib.types.string;
+      type = lib.types.str;
       default = ".config/wallpapers/";
       example = lib.literalExample ''
         .config/wallpapers/
@@ -31,12 +31,10 @@ in {
     colorscheme = lib.mkOption {
       description = ''
         The colorscheme to convert the wallpapers into.
+        Can be a string like "catppuccin-mocha" or a list of strings containing each hex code to use.
       '';
-      type = lib.types.string;
+      type = with lib.types; oneOf [ str (listOf str) ];
       default = "";
-      example = lib.literalExample ''
-        "catppuccin-mocha"
-      '';
     };
     onChange = lib.mkOption {
       type = lib.types.lines;
@@ -54,7 +52,14 @@ in {
     home.file."${cfg.outputPath}" = {
       recursive = true;
       source = pkgs.runCommand "recolored-wallpapers" {} ''
-        ${pkgs.lutgen}/bin/lutgen apply ${cfg.wallpapers}/* -p ${cfg.colorscheme} -o $out
+        ${pkgs.lutgen}/bin/lutgen apply ${cfg.wallpapers}/* -o $out ${
+          if builtins.isList cfg.colorscheme
+          # This is really dumb but it's the only way i could think to do this.
+          # If cfg.colorscheme is a list we separate it and wrap each one with ''.
+          # We have to put the ' on the outside and then use `' '` to as a separator to make them individual strings.
+          then "-- '${builtins.concatStringsSep "' '" cfg.colorscheme}'" 
+          else "-p ${cfg.colorscheme}" # Builtin colorscheme
+        }
       '';
       onChange = cfg.onChange;
     };
